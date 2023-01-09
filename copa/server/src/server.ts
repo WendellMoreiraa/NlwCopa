b6ed4117be75cd236c686db616dd21534601e826
Wendell moreira <wendell.ufpel@gmail.com>
2023-01-09 20:35:57 -0300

    Initial commit

---
+++ copa/server/src/server.ts
@@ -0,0 +1,22 @@
+import  Fastify from 'fastify';
+
+
+async function bootstrap() 
+{
+const fastify = Fastify({
+  logger: true,
+});  
+
+fastify.get('/pools/count', () => {
+  return {count:10}
+})
+
+await fastify.listen({ port:8888 })
+
+
+
+}
+  
+
+
+bootstrap();
